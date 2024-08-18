d85c031a240259d74dd6c5d2ff3c69f46c1978b6
turtelneck <65428036+turtelneck@users.noreply.github.com>
2024-08-17 21:53:29 -0500

    Added debug labels w dummy text

--- Flit/ViewController.swift
+++ Flit/ViewController.swift
@@ -12,7 +12,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
     let cityAndCountryCodeLabel = UILabel()
-    let gpsLocationLabel = UILabel()
+    let gpsLatitudeLabel = UILabel()
+    let gpsLongitudeLabel = UILabel()
+    let gpsTimeStampLabel = UILabel()
+    let gpsRadiusOfUncertaintyVerticalLabel = UILabel()
+    let gpsRadiusOfUncertaintyHorizontalLabel = UILabel()
+    let gpsAltitudeLabel = UILabel()
+    let gpsLogicalFloorOfBuildingLabel = UILabel()
+    
     let gloatingLabel = UILabel()
 
     override func viewDidLoad() {
@@ -22,16 +29,47 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        locationManager.distanceFilter = 50.0
         
         // set up cityAndCountryCodeLabel
         cityAndCountryCodeLabel.textAlignment = .center
         cityAndCountryCodeLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(cityAndCountryCodeLabel)
         
-        // set up gpsLocationLabel
-        gpsLocationLabel.textAlignment = .center
-        gpsLocationLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(gpsLocationLabel)
+        // set up gpsLatitudeLabel
+        gpsLatitudeLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(gpsLatitudeLabel)
+        gpsLatitudeLabel.text = "Latitude: 000000000000"
+        
+        // set up gpsLongitudeLabel
+        gpsLongitudeLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(gpsLongitudeLabel)
+        gpsLongitudeLabel.text = "Longitude: 000000000000"
+        
+        // set up gpsTimeStampLabel
+        gpsTimeStampLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(gpsTimeStampLabel)
+        gpsTimeStampLabel.text = "Updated: arstneio"
+        
+        // set up gpsRadiusOfUncertaintyHorizontalLabel
+        gpsRadiusOfUncertaintyHorizontalLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(gpsRadiusOfUncertaintyHorizontalLabel)
+        gpsRadiusOfUncertaintyHorizontalLabel.text = "Uncertainty (H): arstneio"
+        
+        // set up gpsRadiusOfUncertaintyVerticalLabel
+        gpsRadiusOfUncertaintyVerticalLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(gpsRadiusOfUncertaintyVerticalLabel)
+        gpsRadiusOfUncertaintyVerticalLabel.text = "Uncertainty (V): arstneio"
+        
+        // set up gpsAltitudeLabel
+        gpsAltitudeLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(gpsAltitudeLabel)
+        gpsAltitudeLabel.text = "Altitude(m): some Meters"
+        
+        // set up gpsLogicalFloorOfBuildingLabel
+        gpsLogicalFloorOfBuildingLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(gpsLogicalFloorOfBuildingLabel)
+        gpsLogicalFloorOfBuildingLabel.text = "Logical Floor: 34"
         
         // set up gloatingLabel
         gloatingLabel.textAlignment = .center
@@ -40,15 +78,39 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         NSLayoutConstraint.activate([
             cityAndCountryCodeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            cityAndCountryCodeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -20),
+            cityAndCountryCodeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -105),
             cityAndCountryCodeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             cityAndCountryCodeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             
-            gpsLocationLabel.topAnchor.constraint(equalTo: cityAndCountryCodeLabel.bottomAnchor, constant: 20),
-            gpsLocationLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            gpsLocationLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            gpsLatitudeLabel.topAnchor.constraint(equalTo: cityAndCountryCodeLabel.bottomAnchor, constant: 20),
+            gpsLatitudeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            gpsLatitudeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             
-            gloatingLabel.topAnchor.constraint(equalTo: gpsLocationLabel.bottomAnchor, constant: 20),
+            gpsLongitudeLabel.topAnchor.constraint(equalTo: gpsLatitudeLabel.bottomAnchor, constant: 1),
+            gpsLongitudeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            gpsLongitudeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            gpsTimeStampLabel.topAnchor.constraint(equalTo: gpsLongitudeLabel.bottomAnchor, constant: 1),
+            gpsTimeStampLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            gpsTimeStampLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            gpsRadiusOfUncertaintyHorizontalLabel.topAnchor.constraint(equalTo: gpsTimeStampLabel.bottomAnchor, constant: 1),
+            gpsRadiusOfUncertaintyHorizontalLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            gpsRadiusOfUncertaintyHorizontalLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            gpsRadiusOfUncertaintyVerticalLabel.topAnchor.constraint(equalTo: gpsRadiusOfUncertaintyHorizontalLabel.bottomAnchor, constant: 1),
+            gpsRadiusOfUncertaintyVerticalLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            gpsRadiusOfUncertaintyVerticalLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            gpsAltitudeLabel.topAnchor.constraint(equalTo: gpsRadiusOfUncertaintyVerticalLabel.bottomAnchor, constant: 1),
+            gpsAltitudeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            gpsAltitudeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            gpsLogicalFloorOfBuildingLabel.topAnchor.constraint(equalTo: gpsAltitudeLabel.bottomAnchor, constant: 1),
+            gpsLogicalFloorOfBuildingLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            gpsLogicalFloorOfBuildingLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
+            gloatingLabel.topAnchor.constraint(equalTo: gpsLogicalFloorOfBuildingLabel.bottomAnchor, constant: 20),
             gloatingLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             gloatingLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
@@ -61,7 +123,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
         gloatingLabel.attributedText = attributedText
         
-        gpsLocationLabel.text = "Hi, I'm here"
+        gpsLatitudeLabel.text = "Latitude: 000000000000"
     }
     
     
@@ -72,8 +134,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let geocoder = CLGeocoder()
         geocoder.reverseGeocodeLocation(location) { placemarks, error in
             if let placemark = placemarks?.first {
+                // prep info for cityAndCountryCodeLabel
                 let locality = "\(placemark.locality ?? "unknown city")"
                 let isoCountryCode = "\(placemark.isoCountryCode ?? "unknown country")"
+                
+                // prep info for gps labels
+                let gpsLatitude = placemark.location?.coordinate.latitude
+                let gpsLongitude = placemark.location?.coordinate.longitude
+                let gpsTimeStamp = placemark.location?.timestamp
+                let gpsAltitude = placemark.location?.altitude
+                let gpsRadiusOfUncertaintyVertical = placemark.location?.verticalAccuracy
+                let gpsRadiusOfUncertaintyHorizontal = placemark.location?.horizontalAccuracy
+                let gpsLogicalFloorOfBuilding = placemark.location?.floor
+                
                 DispatchQueue.main.async {
                     self.cityAndCountryCodeLabel.text = "Simulated Location: \(locality), \(isoCountryCode)"
                 }
